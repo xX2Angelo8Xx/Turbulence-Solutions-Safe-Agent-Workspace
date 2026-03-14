5f061cbb36bd16d0f72ac04ea1e424c554fba0eb
xX2Angelo8Xx <angelomichaelamon2001@gmail.com>
2026-03-14 17:38:25 +0100

    Release v1.0.0

--- src/launcher/config.py
+++ src/launcher/config.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 APP_NAME: str = "Turbulence Solutions Launcher"
-VERSION: str = "0.1.0"
+VERSION: str = "1.0.0"
 COLOR_PRIMARY: str = "#0A1D4E"
 COLOR_SECONDARY: str = "#5BC5F2"
 COLOR_TEXT: str = "#FFFFFF"
